4e2a69589c48ee8b6fac5d6bb6eddd9eaa9f7782
Vinicius Chagas <vinicius.chagas@g.globo>
2023-05-10 10:22:54 -0300

    add formsubmit.co

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,5 @@
-
 const form = document.querySelector('#form');
 
 form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  
-  let answers = {
-    formato: document.getElementById('formato-material').value,
-    e_dt_comemorativa: document.getElementById('e-dt-comemorativa').value,
-    dt_comemorativa: (document.getElementById('dt-comemorativa').value).split('-').reverse().join('/'),
-    titulo: document.getElementById('titulo-material').value,
-    legenda: document.getElementById('legenda-material').value,
-    observacoes: document.getElementById('observacoes').value,
-    anexos: document.getElementById('anexo').files
-  }
-
-  console.log(answers);
+  alert('Obrigado! As informações foram enviadas com sucesso para o e-mail contato@kbrumstudio.com.br.')
 });
